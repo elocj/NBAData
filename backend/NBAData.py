8ef2057a67d37c73bb796e40bbb6965a170097b2
Anthony Nicholas Joo -X (ajoo - GLOW NETWORKS INC at Cisco) <ajoo@cisco.com>
2019-08-26 16:59:41 -0700

    third

--- backend/NBAData.py
+++ backend/NBAData.py
@@ -1,7 +1,10 @@
 import os
 
+import requests
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import json
 from flask import request
 from flask import redirect
 
@@ -10,18 +13,24 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    num = None
-    if request.form:
-        num = request.form.get("num")
-        num = turnToArray(num)
-    return render_template("index.html", num = num)
+    return render_template("index.html")
 
-def turnToArray(num):
-    arr = num.split(',')
-    try:
-        return [int(value) for value in arr]
-    except Exception as e:
-        print("Cannot typecast non-numbers")
-        print(e)
+@app.route('/get_data', methods = ['GET', 'POST'])
+def get_data():
+    r = requests.get("https://www.balldontlie.io/api/v1/stats?player_ids[]=115&per_page=25").json()
+    arr = []
+    date = []
+    for data in r['data']:
+        arr.append([data['pts'], data['reb'], data['stl'], data['turnover'], data['ast'], data['blk']])
+        day = data['game']['date'].split('T')
+        date.append(day[0].split('-'))
+    arr = calcScore(arr)
+    return jsonify({'payload': {'date': date, 'arr': arr}})
+
+def calcScore(arr):
+    score = []
+    for pts, reb, stl, turnover, ast, blk in arr:
+        score.append(pts * 1 + reb * 1.2 + stl * 3 + turnover * -1 + ast * 1.5 + blk * 3)
+    return score
 
 app.run()
---
+++ frontend/static/js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $.get("http://127.0.0.1:5000/get_data", function(data, status){
+        // data = JSON.parse(data);
+        alert("Data: " + JSON.stringify(data.payload.date) + "\nStatus: " + status);
+        date = data.payload.date;
+        arr = data.payload.arr;
+        var dataPoints = [];
+        for (var i = 0; i < date.length; i++) {
+            dataPoints.push({
+                x: new Date(date[i][0], date[i][1], date[i][2]),
+                y: arr[i]
+            });
+        }
+        var chart = new CanvasJS.Chart("chartContainer",
+            {
+                title:{
+                    text: "Steph Curry with the shot boy"
+                },
+                data: [{
+                    type: "line",
+                    dataPoints: dataPoints
+                }
+            ]
+        });
+        chart.render();
+    });
+});
